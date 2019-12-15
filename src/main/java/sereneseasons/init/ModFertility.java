e7e12a20b6fbf9c6c63727cc2a30d8fe9465b96c
Adubbz <Adubbz@users.noreply.github.com>
2019-12-15 11:24:04 +1100

    Oops

--- src/main/java/sereneseasons/init/ModFertility.java
+++ src/main/java/sereneseasons/init/ModFertility.java
@@ -209,6 +209,8 @@ public class ModFertility
             {
                 int mask = seedSeasons.get(name);
 
+                event.getToolTip().add(new StringTextComponent("Fertile Seasons:"));
+
                 if ((mask & 1) != 0 && (mask & 2) != 0 && (mask & 4) != 0 && (mask & 8) != 0)
                 {
                     event.getToolTip().add(new StringTextComponent(TextFormatting.LIGHT_PURPLE + " Year-Round"));
