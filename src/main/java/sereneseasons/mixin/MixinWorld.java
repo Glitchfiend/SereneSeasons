1863189d8b6d5615ab89e3526c93bf3c714e94ca
Adubbz <Adubbz@users.noreply.github.com>
2021-07-25 12:19:14 +1000

    Updated license headers

--- src/main/java/sereneseasons/mixin/MixinWorld.java
+++ src/main/java/sereneseasons/mixin/MixinWorld.java
@@ -1,10 +1,7 @@
-///*******************************************************************************
-// * Copyright 2014-2020, the Serene Seasons Team
-// *
-// * This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License.
-// *
-// * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
-// ******************************************************************************/
+/*******************************************************************************
+ * Copyright 2021, the Glitchfiend Team.
+ * All rights reserved.
+ ******************************************************************************/
 //package sereneseasons.mixin;
 //
 //import net.minecraft.resources.ResourceKey;
