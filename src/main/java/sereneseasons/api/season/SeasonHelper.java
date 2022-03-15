1fc1ffa889ffb4e0cd8a42e1445c4ae9ef3dcc56
Adubbz <Adubbz@users.noreply.github.com>
2022-03-15 11:19:32 +1100

    Updated for 1.18.2

--- src/main/java/sereneseasons/api/season/SeasonHelper.java
+++ src/main/java/sereneseasons/api/season/SeasonHelper.java
@@ -4,7 +4,10 @@
  ******************************************************************************/
 package sereneseasons.api.season;
 
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 
 public class SeasonHelper 
 {
@@ -30,9 +33,20 @@ public class SeasonHelper
         return data;
     }
 
+    /**
+     * Check whether a biome uses tropical seasons.
+     * @param biome the biome to check.
+     * @return whether the biome uses tropical seasons.
+     */
+    public static boolean usesTropicalSeasons(Holder<Biome> biome)
+    {
+        return dataProvider.usesTropicalSeasons(biome);
+    }
+
     public interface ISeasonDataProvider
     {
         ISeasonState getServerSeasonState(Level world);
         ISeasonState getClientSeasonState();
+        boolean usesTropicalSeasons(Holder<Biome> key);
     }
 }
