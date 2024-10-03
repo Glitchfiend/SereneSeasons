01b749fb57a3e75fde1337ee2c264a01c33fca5e
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-10-03 08:30:21 +0200

    fix imports here

--- common/src/main/java/sereneseasons/block/SeasonSensorBlock.java
+++ common/src/main/java/sereneseasons/block/SeasonSensorBlock.java
@@ -4,22 +4,21 @@
  ******************************************************************************/
 package sereneseasons.block;
 
-import javax.annotation.Nullable;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -28,6 +27,8 @@ import sereneseasons.api.season.SeasonHelper;
 import sereneseasons.block.entity.SeasonSensorBlockEntity;
 import sereneseasons.init.ModConfig;
 import sereneseasons.season.SeasonTime;
+
+import javax.annotation.Nullable;
 
 public class SeasonSensorBlock extends BaseEntityBlock
 {
