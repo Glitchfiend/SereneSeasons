dccb9140afa8ed05182cbee9a799193ac1ab527a
ZZZank <47418975+ZZZank@users.noreply.github.com>
2025-03-02 06:19:46 +0000

    add subseason color config

--- common/src/main/java/sereneseasons/config/SeasonsConfig.java
+++ common/src/main/java/sereneseasons/config/SeasonsConfig.java
@@ -149,8 +149,23 @@ public class SeasonsConfig extends glitchcore.config.Config
         return seasonPropertiesMapper.get().get(season);
     }
 
-    public record SeasonProperties(Season.SubSeason subSeason, float meltChance, int meltRolls, float biomeTempAdjustment, int minRainTime, int maxRainTime, int minThunderTime, int maxThunderTime)
-    {
+    public record SeasonProperties(
+        // season
+        Season.SubSeason subSeason,
+        // server side properties
+        float meltChance, int meltRolls, float biomeTempAdjustment, int minRainTime, int maxRainTime, int minThunderTime, int maxThunderTime,
+        // client side properties
+        int grassColour, float grassSaturation, int foliageColour, float foliageSaturation, int birchColor
+    ) {
+        public SeasonProperties(
+            // season
+            Season.SubSeason subSeason,
+            // server side properties
+            float meltChance, int meltRolls, float biomeTempAdjustment, int minRainTime, int maxRainTime, int minThunderTime, int maxThunderTime
+        ) {
+            this(subSeason, meltChance, meltRolls, biomeTempAdjustment, minRainTime, maxRainTime, minThunderTime, maxThunderTime, subSeason.getGrassOverlay(), subSeason.getGrassSaturationMultiplier(), subSeason.getFoliageOverlay(), subSeason.getFoliageSaturationMultiplier(), subSeason.getBirchColor());
+        }
+
         public Config encode()
         {
             Config config = Config.of(LinkedHashMap::new, InMemoryFormat.withUniversalSupport());
@@ -162,6 +177,11 @@ public class SeasonsConfig extends glitchcore.config.Config
             config.add("max_rain_time", this.maxRainTime);
             config.add("min_thunder_time", this.minThunderTime);
             config.add("max_thunder_time", this.maxThunderTime);
+            config.add("grass_colour", this.grassColour);
+            config.add("grass_saturation", this.grassSaturation);
+            config.add("foliage_colour", this.foliageColour);
+            config.add("foliage_saturation", this.foliageSaturation);
+            config.add("birch_color", this.birchColor);
             return config;
         }
 
@@ -177,14 +197,28 @@ public class SeasonsConfig extends glitchcore.config.Config
                 int maxRainTime = config.getInt("max_rain_time");
                 int minThunderTime = config.getInt("min_thunder_time");
                 int maxThunderTime = config.getInt("max_thunder_time");
+                int grassColour = config.getInt("grass_colour");
+                float grassSaturation = config.<Number>get("grass_saturation").floatValue();
+                int foliageColour = config.getInt("foliage_colour");
+                float foliageSaturation = config.<Number>get("foliage_saturation").floatValue();
+                int birchColor = config.getInt("birch_color");
 
                 Preconditions.checkArgument(meltChance >= 0.0F && meltChance <= 100.0F);
                 Preconditions.checkArgument(rolls >= 0);
                 Preconditions.checkArgument(biomeTempAdjustment >= -10.0 && biomeTempAdjustment <= 10.0);
                 Preconditions.checkArgument(minRainTime <= maxRainTime);
                 Preconditions.checkArgument(minThunderTime <= maxThunderTime);
+                Preconditions.checkArgument(grassColour >= 0 && grassColour <= 0xFFFFFF);
+                Preconditions.checkArgument(grassSaturation >= 0);
+                Preconditions.checkArgument(foliageColour >= 0 && foliageColour <= 0xFFFFFF);
+                Preconditions.checkArgument(foliageSaturation >= 0);
+                Preconditions.checkArgument(birchColor >= 0 && birchColor <= 0xFFFFFF);
 
-                return Optional.of(new SeasonProperties(subSeason, meltChance, rolls, biomeTempAdjustment, minRainTime, maxRainTime, minThunderTime, maxThunderTime));
+                return Optional.of(new SeasonProperties(
+                    subSeason,
+                    meltChance, rolls, biomeTempAdjustment, minRainTime, maxRainTime, minThunderTime, maxThunderTime,
+                    grassColour, grassSaturation, foliageColour, foliageSaturation, birchColor
+                ));
             }
             catch (Exception e)
             {
