b939ebca3ca567050e896b1fee61e88ad2406347
ZZZank <47418975+ZZZank@users.noreply.github.com>
2025-03-02 07:15:53 +0000

    correct comments

--- common/src/main/java/sereneseasons/config/SeasonsConfig.java
+++ common/src/main/java/sereneseasons/config/SeasonsConfig.java
@@ -123,9 +123,9 @@ public class SeasonsConfig extends glitchcore.config.Config
                 min_thunder_time is the minimum time interval between thunder events in ticks. Set to -1 to disable thunder.
                 max_thunder_time is the maximum time interval between thunder events in ticks. Set to -1 to disable thunder.
                 grass_colour is the color of grass, from 0 to 0xFFFFFF(16777215).
-                grass_saturation is the saturation multiplier of grass color.
+                grass_saturation is the saturation multiplier of grass color. Set to -1 to disable.
                 foliage_colour is the color of foliage, from 0 to 0xFFFFFF(16777215).
-                foliage_saturation is the saturation multiplier of foliage color.
+                foliage_saturation is the saturation multiplier of foliage color. Set to -1 to disable.
                 birch_color is the color of birch foliage, from 0 to 0xFFFFFF(16777215). It will use the same saturation multiplier of foliage_colour""", SEASON_PROPERTIES_VALIDATOR);
 
         seasonPropertiesMapper = Suppliers.memoize(() -> {
